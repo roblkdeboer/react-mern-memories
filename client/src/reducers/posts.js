3623d7a7bd065a2181910d6a443de3f367224fc1
Rob de Boer <roblkdeboer@gmail.com>
2021-04-21 21:47:29 +0800

    Fix typo in reducers for delete action

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,16 +3,18 @@ import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 // What to do when an action is called
 // Posts will be an array
 export default (posts = [], action) => {
-    switch (action.type) {
-        case DELETE:
-            return posts.filter((post) => post._id !== action.payloa)
-        case UPDATE:
-            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
-        case FETCH_ALL:
-            return action.payload;
-        case CREATE:
-            return [...posts, action.payload];
-        default:
-            return posts;
-    }
-}
+  switch (action.type) {
+    case DELETE:
+      return posts.filter((post) => post._id !== action.payload);
+    case UPDATE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case FETCH_ALL:
+      return action.payload;
+    case CREATE:
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
